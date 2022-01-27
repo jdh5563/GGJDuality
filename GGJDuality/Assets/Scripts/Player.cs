c9594d31d948019a202cb337c01b498abc401008
John Heiden <jdh5563@rit.edu>
2022-01-27 12:17:23 -0500

    Created code stubs for player input

--- GGJDuality/Assets/Scripts/Player.cs
+++ GGJDuality/Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Fields
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PollInput();
     }
+
+    private void PollInput()
+	{
+        if(Input.GetKeyDown(KeyCode.W))
+		{
+            // Jump
+		}
+
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+            // Move Left
+		}
+        else if (Input.GetKeyDown(KeyCode.D))
+		{
+            // Move Right
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+            // Toggle Light/Dark Mode
+		}
+	}
 }
